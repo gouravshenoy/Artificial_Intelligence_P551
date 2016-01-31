23e66ef66c492d36816ba3f625761334d1d9fe76
Gourav Shenoy <shenoy.200@gmail.com>
2016-01-30 21:16:19 -0500

    Implement Iterative Deepening Search

--- graph_traversal/graph_traversal.py
+++ graph_traversal/graph_traversal.py
@@ -10,11 +10,11 @@ class Graph_Node:
     Class represents node in a graph
     """
 
-    def __init__(self, name, path, cost, visited):
+    def __init__(self, name, path, cost, level=None):
         self.name = name
         self.path = path
         self.cost = cost
-        self.visited = visited
+        self.level = level
 
 
 class Graph_Traversal:
@@ -41,7 +41,7 @@ class Graph_Traversal:
         with open(ip_file) as fp:
             for line in fp:
                 line = line.rstrip()
-                cities = line.split(' ')
+                cities = line.split(',')
                 city_1 = cities[0].lower()
                 city_2 = cities[1].lower()
                 cost = int(cities[2])
@@ -88,11 +88,15 @@ class Graph_Traversal:
                 self.dfs_search(origin=origin,
                                 destination=destination,
                                 graph_dict=graph_dict)
+            elif algorithm == 'iterative deepening':
+                self.iterative_deepening_search(origin=origin,
+                                                destination=destination,
+                                                graph_dict=graph_dict)
             else:
                 print('Invalid Search Entered!')
 
         except Exception as ex:
-            print('Something went wrong with the search. Try again!')
+            print('Something went wrong with the search. Try again!' , ex.message)
             return
 
     @classmethod
@@ -112,8 +116,7 @@ class Graph_Traversal:
         # create a node
         node = Graph_Node(name=origin,
                           path=origin,
-                          cost=0,
-                          visited=True)
+                          cost=0)
 
         # enqueue city node
         queue.put(node)
@@ -131,7 +134,7 @@ class Graph_Traversal:
 
             # check if goal state
             if city_name == destination:
-                print("Reached destination, Path = " + current_path + ', Cost = ' + str(current_cost))
+                print("Reached destination, Path = [" + current_path + '], Cost = ' + str(current_cost))
                 return
 
             # get neighbouring cities
@@ -144,8 +147,7 @@ class Graph_Traversal:
                 # if not, create a new node & enqueue
                 node = Graph_Node(name=city,
                                   path=current_path + ',' + city,
-                                  cost=(current_cost + cost),
-                                  visited=False)
+                                  cost=(current_cost + cost))
                 # enque node
                 queue.put(node)
                 # end-for-loop
@@ -170,8 +172,7 @@ class Graph_Traversal:
         # create a node
         node = Graph_Node(name=origin,
                           path=origin,
-                          cost=0,
-                          visited=True)
+                          cost=0)
 
         # push city node
         stack.append(node)
@@ -189,7 +190,7 @@ class Graph_Traversal:
 
             # check if goal state
             if city_name == destination:
-                print("Reached destination, Path = " + current_path + ', Cost = ' + str(current_cost))
+                print("Reached destination, Path = [" + current_path + '], Cost = ' + str(current_cost))
                 return
 
             # get neighbouring cities
@@ -202,8 +203,7 @@ class Graph_Traversal:
                 # if not, create a new node & enqueue
                 node = Graph_Node(name=city,
                                   path=current_path + ',' + city,
-                                  cost=(current_cost + cost),
-                                  visited=False)
+                                  cost=(current_cost + cost))
                 # push node
                 stack.append(node)
                 # end-for-loop
@@ -211,6 +211,77 @@ class Graph_Traversal:
 
         return None
 
+    @classmethod
+    def iterative_deepening_search(self, origin, destination, graph_dict):
+        """
+        Method performs Iterative Deepening Search
+        using queues
+        :param origin:
+        :param destination:
+        :return:
+        """
+
+        # Initialize iteration level
+        iteration_level = 1
+
+        # keep iterating
+        while(True):
+            # Basic data structures required for Iterative Deepening
+            queue = Queue()
+            visited_list = []
+
+            # create a node
+            node = Graph_Node(name=origin,
+                              path=origin,
+                              cost=0,
+                              level=1)
+
+            # enqueue city node
+            queue.put(node)
+
+            # perform till queue is empty
+            while queue.empty() is False:
+                # get next node
+                current_node = queue.get()
+                city_name = current_node.name
+                current_path = current_node.path
+                current_cost = current_node.cost
+
+                # calculate node level
+                current_level = len(current_path.rstrip().split(','))
+
+                # add to visited list
+                visited_list.append(city_name)
+
+                # check if goal state
+                if city_name == destination:
+                    print("Reached destination, Path = [" + current_path + '], Cost = ' + str(current_cost) + ', Iteration Level = ' + str(iteration_level))
+                    return
+
+                # check iteration level
+                if current_level == iteration_level:
+                    iteration_level += 1
+                    continue
+
+                # get neighbouring cities
+                neighbors = graph_dict[city_name]
+                for city, cost in neighbors.iteritems():
+                    # check if city is already visited
+                    if city in visited_list:
+                        continue
+
+                    # if not, create a new node & enqueue
+                    node = Graph_Node(name=city,
+                                      path=current_path + ',' + city,
+                                      cost=(current_cost + cost),
+                                      level=current_level + 1)
+                    # enque node
+                    queue.put(node)
+                    # end-for-loop
+            # end-inner-while-loop
+        # end-outer-while-loop
+
+        return None
 
 if __name__ == '__main__':
     # define the arg parser
@@ -223,13 +294,26 @@ if __name__ == '__main__':
     ip_file = os.path.realpath(
         os.path.expanduser(ip_file))
 
-    # get user input
-    origin = raw_input("Enter source city: ")
-    destination = raw_input("Enter destination city: ")
-    algorithm = raw_input("Enter the search technique (BFS/DFS/Iterative Deepening): ")
+    # re-run till user choice
+    while(True):
+        # get user input
+        origin = raw_input("Enter source city: ")
+        destination = raw_input("Enter destination city: ")
+        algorithm = raw_input("Enter the search technique (BFS/DFS/Iterative Deepening): ")
 
-    # call the class method
-    graph_traversal = Graph_Traversal(ip_file)
-    graph_traversal.find_path(origin=origin.lower(),
-                              destination=destination.lower(),
-                              algorithm=algorithm.lower())
+        # call the class method
+        graph_traversal = Graph_Traversal(ip_file)
+        graph_traversal.find_path(origin=origin.rstrip().lower(),
+                                  destination=destination.rstrip().lower(),
+                                  algorithm=algorithm.rstrip().lower())
+
+        print("************** COMPLETE ****************")
+        # check if user wants to continue
+        repeat = raw_input("Do you wish to re-run the program? (Yes/No): ")
+
+        # break if user wishes to discontinue
+        if (repeat.rstrip().lower() == 'no') or \
+                (repeat.rstrip().lower() == 'n'):
+            print("Exiting Now!")
+            break
+    #end-while-loop
